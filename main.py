c017a19b51ea1e21a59da9bb5bcef004aa4381f6
WilliamYu <562937707@qq.com>
2022-02-23 07:48:41 +0800

    can not swipe like button.

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ driver.get(TINDER_URL)
 time.sleep(5)
 
 # todo 1 click login
-login_button = driver.find_element(By.XPATH, '//*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/span')
+login_button = driver.find_element(By.XPATH,
+                                   '//*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/span')
 login_button.click()
 
 time.sleep(2)
 
 # todo 2 click login with facebook
-login_with_facebook_button = driver.find_element(By.XPATH, '//*[@id="o-1687095699"]/div/div/div[1]/div/div[3]/span/div[2]/button/span[2]')
+login_with_facebook_button = driver.find_element(By.XPATH,
+                                                 '//*[@id="o-1687095699"]/div/div/div[1]/div/div[3]/span/div[2]/button/span[2]')
 login_with_facebook_button.click()
 
 time.sleep(10)
@@ -63,7 +65,8 @@ location_button.click()
 time.sleep(random.uniform(1, 2))
 
 # click not allow notification anchor
-not_allow_notification_anchor = driver.find_element(By.XPATH, '//*[@id="o-1687095699"]/div/div/div/div/div[3]/button[2]')
+not_allow_notification_anchor = driver.find_element(By.XPATH,
+                                                    '//*[@id="o-1687095699"]/div/div/div/div/div[3]/button[2]')
 not_allow_notification_anchor.click()
 
 time.sleep(random.uniform(1, 2))
@@ -78,17 +81,45 @@ for n in range(100):
     # print(f"pick window:\ntitle->{driver.title}\nname->{driver.window_handles}")
     time.sleep(random.uniform(1, 2))
     try:
-        pick_button = driver.find_element(By.XPATH, '//*[@id="q1323319974"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
-        pick_button.click()
-        print("clicked like.")
+        if n == 0:
+            pick_button = driver.find_element(By.XPATH, '//*[@id="o41285377"]/div/div[1]/div/div/main/div/div/div[1]/div/div[4]/div/div[4]/button')
+
+        else:
+            # short
+            # //*[@id="o41285377"]/div/div[1]/div/div/main/div/div/div[1]/div/div[4]/div/div[4]/button
+            # //*[@id="o41285377"]/div/div[1]/div/div/main/div/div/div[1]/div/div[5]/div/div[4]/button
+            # //*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button works after a wipe
+            # //*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button
+
+            # long
+            # //*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button
+            # //*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]/button
+
+            pick_button = driver.find_element(By.XPATH, '//*[@id="o41285377"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]/button')
+
+            pick_button.click()
+            print(n)
+            print("clicked like.")
     # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except NoSuchElementException:
+        time.sleep(random.uniform(2, 3))
+        print("do not find like button.")
+    except ElementClickInterceptedException:
         try:
-            match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
+            refuse_add_desk = driver.find_element(By.XPATH, '//*[@id="o-1687095699"]/div/div/div[2]/button[2]')
+            refuse_add_desk.click()
+            print("refuse add to desktop.")
+        except ElementClickInterceptedException:
+            # match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
+            match_popup = driver.find_element(By.XPATH,
+                                              '//*[@id="o-1687095699"]/div/div/div[1]/div/div[4]/button/svg/path')
             match_popup.click()
             print("clicked match.")
-        except NoSuchElementException:
-            time.sleep(random.uniform(2, 3))
-            print("do not find like button.")
 
 driver.quit()
+
+# some like you and click xx
+# //*[@id="o-1687095699"]/div/div/div[1]/div/div[4]/button/svg/path
+
+# add to screen -- do not interested
+# //*[@id="o-1687095699"]/div/div/div[2]/button[2]
